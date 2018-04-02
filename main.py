ca52a50f5af91c7f5f73c3dcc713536d994de09f
Navras Kamal <32576299+NavrasK@users.noreply.github.com>
2018-04-02 14:10:45 -0600

    setting up the block generation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib as hasher
-import time as timer
+import hashlib as hasher #for SHA256
+import time as timer #UNIX time, since actual date/time is irrellevant compared to relative time
+import random
 
 '''
 TODO: 
@@ -9,18 +10,28 @@ Create consensus block chain.  This will be a list of transactions which when fo
 Create transaction tree.  This will be a tree object which will take in all the incoming transactions and will be used to determine what to add to block chain
 '''
 
-class Genisis():
-    def __init__(self, index, origin_time, time, data, hash):
-        self.index = 0
-        self.origin_time = timer.time()
-        self.time = 0
+class Genesis():
+    def __init__(self, index, origin_time, timestamp, data, hash):
+        self.index = 0                      #Genesis block is block with index 0
+        self.origin_time = timer.time()     #Origin time is the actual time when the chain started
+        self.timestamp = 0                  #Time is the relative time from the origin time in seconds
 
-class Block():
-    def __init__(self, index, time, data, previous_hash, hash):
+class XP(): #The blocks in the chain are called XP
+    def __init__(self, index, timestamp, data, previous_hash, hash):
         self.index = index
-        self.time = timer.time() - AcceptedChain[0].origin_time
+        self.timestamp = timer.time() - AcceptedChain[0].origin_time
         self.data = data
         self.previous_hash = previous_hash
+        self.hash = self.hash_xp()
+    def hash_xp(self):
+        sha256 = hasher.sha256()
+        sha256.update(str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.getNonce(random.randint(7,10))))
+        return sha256.hexdigest()
+    def getNonce(self, len):
+        nonce = ''
+        for _ in range(len):
+            nonce.join(str(random.randint(0,9)))
+        return str(nonce)
 
 AcceptedChain = list()
 
