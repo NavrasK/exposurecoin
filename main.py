504ce856ff8cbe86d863ff14b9011b9d29e49e8b
Navras Kamal <32576299+NavrasK@users.noreply.github.com>
2018-04-03 00:19:21 -0600

    working on it, need to figure out user ids ASAP

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ Create consensus block chain.  This will be a list of transactions which when fo
 Create transaction tree.  This will be a tree object which will take in all the incoming transactions and will be used to determine what to add to block chain
 '''
 
+userID = 123456789 #Not sure how to handle this yet but it's super important... it will track your owned currency and your actions
+
 class Genesis():
     def __init__(self, index, origin_time, timestamp, data, hash):
         self.index = 0                      #Genesis block is block with index 0
@@ -17,12 +19,21 @@ class Genesis():
         self.timestamp = 0                  #Time is the relative time from the origin time in seconds
 
 class XP(): #The blocks in the chain are called XP
-    def __init__(self, index, timestamp, data, previous_hash, hash):
+    def __init__(self, index, timestamp, data, previous_hash, hash, minted, bounty, minerID):
         self.index = index
         self.timestamp = timer.time() - AcceptedChain[0].origin_time
         self.data = data
         self.previous_hash = previous_hash
-        self.hash = self.hash_xp()
+        self.minted = False
+        self.hash = self.grind_xp()
+    def grind_xp(self):
+        while(self.minted == False):
+            test_hash = self.hash_xp()
+            if (test_hash[0]=='a' and test_hash[1]=='b' and test_hash[2]=='c' and test_hash[3]=='1' and test_hash[4]=='2' and test_hash[5]=='3'):
+                if self.minted == False:
+                    self.minted = True
+                    self.minerID = userID
+                    return test_hash
     def hash_xp(self):
         sha256 = hasher.sha256()
         sha256.update(str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.getNonce(random.randint(7,10))))
