919ade1fed97d1e98d2456112eb13bfd89607de2
Navras Kamal <32576299+NavrasK@users.noreply.github.com>
2018-04-04 22:46:01 -0600

    yaml loader working for confidential credentials.  To use yourself, get your API credential from Pastebin and add it to credentials.yaml.example, then remove the .example extension

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 from pastebin import PasteBin
+import yaml
 
 class Server():
     def __init__(self):
         url1 = "idk"
+    def get_pastebin_dev_key(self):
+        with open("credentials.yaml", "r") as file:
+            data = yaml.load(file)
+        keys = data.get('keys')
+        id = keys.get('pastebin_dev_api')
+        return id
+    
