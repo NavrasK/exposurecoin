b13c06c95104bee1edb6991bba45e999b242e438
Navras Kamal <32576299+NavrasK@users.noreply.github.com>
2018-04-05 08:53:01 -0600

    -

--- networking_.py
+++ networking_.py
@@ -1,3 +1,5 @@
 # Handles inter-computer communication
 
 # TODO: Everything
+
+# Note the information to be transmitted include at least the ending of new pastebin URLs along with an identifier to show which file is being updated, and a request function which causes all other devices on the network to pass in their stored values
