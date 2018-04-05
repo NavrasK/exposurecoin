16c6744ec5be2d980f6afc2b2abd2c43c69e2b38
Navras Kamal <32576299+NavrasK@users.noreply.github.com>
2018-04-04 21:55:04 -0600

    working with credentials

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
-from urllib.request import urlopen
-from urllib.request import urlretrieve
+from pastebin import PasteBin
 
-text = urlopen("http://www.brokenmirror.ca/test.txt")
-for line in text:
-    print(str(line.rstrip().decode()))
-
+class Server():
+    def __init__(self):
+        
