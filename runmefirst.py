23c9a27b86af2b18a12b4e034bf913c220f1b080
NavrasK <32576299+NavrasK@users.noreply.github.com>
2018-04-09 22:55:47 -0600

    user friendliness

--- runmefirst.py
+++ runmefirst.py
@@ -1,10 +1,12 @@
 import os
+import time
 from client import main
 
 try:
     import msgpack
 except ImportError:
+    print("Installing msgpack-python in 3 seconds [quit via interrupt to cancel]")
+    time.sleep(4)
     os.system('sudo pip3 install msgpack-python')
-    print("Installing msgpack-python")
 
 main()
