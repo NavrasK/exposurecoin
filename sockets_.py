2a0539dfbf47415de18ab709b56e9a3b206a1a60
Navras Kamal <32576299+NavrasK@users.noreply.github.com>
2018-04-09 19:37:36 -0600

    not much

--- sockets_.py
+++ sockets_.py
@@ -1,27 +1,50 @@
-# Now you're thinking with Sockets (testing file)
+# The custom sockets system for networking purposes
 
-# Tutorials from sentdex (https://www.youtube.com/channel/UCfzlCWGWYyIQ0aLC5w48gBQ) and 
-# Gareth Nelson (https://steemit.com/cryptocurrency/@garethnelsonuk/creating-a-toy-cryptocurrency-part-2a-p2p-networking)
+# Currently a test file for working, successful methods will be addded to networking_ before final build
+
+# Tutorials from sentdex (https://www.youtube.com/channel/UCfzlCWGWYyIQ0aLC5w48gBQ) and Gareth Nelson
+# (https://steemit.com/cryptocurrency/@garethnelsonuk/creating-a-toy-cryptocurrency-part-2a-p2p-networking)
 
 import socket
 import pprint
+import threading as thread
 try:
     import msgpack
 except:
     raise ImportError("Install msgpack with 'pip install msgpack-python'")
 
-example_files = {'testing.txt':'This is a tessst', "linkinpark.mp3.exe":"100 percent legit"}
+# example_files = {'testing.txt':'This is a tessst', "linkinpark.mp3.exe":"100 percent legit"}
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', 0))
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.bind(('', 0))
 
-print("Listening for peers @ UDP port %s" % s.getsockname()[1])
+# print("Listening for peers @ UDP port %s" % s.getsockname()[1])
 
-while True:
-    in_data, in_addr = s.recvfrom(65536)
-    print("Got a packet from %s" % str(in_addr))
-    try:
-        data = msgpack.unpackb(in_data)
-        pprint.pprint(data)
-    except:
-        print("ERROR: Could not unpack from peer")
+# while True:
+#     in_data, in_addr = s.recvfrom(65536)
+#     print("Got a packet from %s" % str(in_addr))
+#     try:
+#         data = msgpack.unpackb(in_data)
+#         pprint.pprint(data)
+#     except:
+#         print("ERROR: Could not unpack from peer")
+
+class FileShareNode:
+   def __init__(self, data):
+       self.file_content = data
+       self.s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+       self.s.bind(('',0))
+       print('Listening for peers @ UDP port %s' % self.s.getsockname()[1])
+       thread._start_new_thread(self._sock_thread,())
+   def handle_ping(self, in_data, addr):
+       reply_data = msgpack.packb({'msgtype':'pong', 'replyto':in_data['msgid']})
+       self.s.sendto(reply_data, addr)
+   def _sock_thread(self):
+       while True:
+         in_data, in_addr = self.s.recvfrom(65536) 
+         data = msgpack.unpackb(in_data)
+         pprint.pprint((in_addr, data))
+         getattr(self,'handle_%s' % data['msgtype'])(data, in_addr)
+
+if __name__ == "__main__":
+    node = FileShareNode({})
