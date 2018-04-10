c4311b6c16081fe86b3aafcffa7e02d0d42f0dd4
Navras Kamal <32576299+NavrasK@users.noreply.github.com>
2018-04-09 23:55:38 -0600

    Base network finished

--- networking_.py
+++ networking_.py
@@ -3,7 +3,27 @@
 # This only handles local networking and is purely for demonstration, 
 # as interdevice communication did not work on the VM
 
+import socket
+import pprint
+import threading as thread
+try:
+    import msgpack 
+except:
+    raise ImportError("Install msgpack with 'sudo pip3 install msgpack-python'")
+
 class Network():
     def __init__(self):
         self.ip = '127.0.0.1' # Localhost
         self.peers = {}
+        self.print_lock = thread.Lock()
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.bind(('', 0))
+        with self.print_lock:
+            print('Listening for peers @ UDP port %s' % self.s.getsockname()[1])
+        thread._start_new_thread(self.socket_thread, ())
+    def socket_thread(self):
+        while True:
+            in_data, in_addr = self.s.recvfrom(65536)
+            data = msgpack.unpackb(in_data)
+            with self.print_lock:
+                pprint.pprint((in_addr, data))
