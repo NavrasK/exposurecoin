b326300bd147e267fa50b101a715e0b3015ac494
Jesse <jesse.goertzen2@gmail.com>
2018-04-10 02:58:04 -0600

    fixes maybe

--- xp_.py
+++ xp_.py
@@ -23,11 +23,6 @@ Create consensus block chain.  This will be a list of transactions which when fo
 Create transaction tree.  This will be a tree object which will take in all the incoming transactions and will be used to determine what to add to block chain
 '''
 
-
-    
-
-userID = str(input()) #Not sure how to handle this yet but it's super important... it will track your owned currency and your actions
-
 def threaded(fn):
     def wrapper(*args, **kwargs):
         threading.Thread(target=fn, args=args, kwargs=kwargs).start()
@@ -41,42 +36,6 @@ def createGenesis():
 
     return {u'hash':hash, u'contents':contents}
 
-class XP():
-    # The blocks in the chain are called XP
-    
-    def __init__(self, index, timestamp, data, previous_hash, hash, minted, bounty, minerID):
-        self.tree = WorkingTree()
-        self.index = index
-        self.timestamp = timer.time() #TODO: same thing as Genesis
-        self.data = data
-        self.transactions = {}
-        self.previous_hash = previous_hash
-        self.minted = False
-        self.hash = self.grind_xp()
-        self.k = Keys()
-
-    def grind_xp(self):
-        # Tries to find the SHA256 Hash which begins with the sequence 'abc123'
-        # TODO Find the probability of this happening for mining time analysis
-
-        while(self.minted == False):
-            test_hash = self.k.encrypt(self)
-            if (test_hash[0]=='a' and test_hash[1]=='b' and test_hash[2]=='c' \
-                and test_hash[3]=='1' and test_hash[4]=='2' and test_hash[5]=='3'):
-                if self.minted == False:
-                    self.minted = True
-                    self.minerID = userID
-                    return test_hash
-
-    def spinner(self):
-        # Displays a spinning cursor while mining to show a process is running
-        # TODO probably want to multi-thread this otherwise this will cause mining to take MUCH longer
-
-        while True:
-            for cursor in '-\\|/':
-                time.sleep(0.1)
-                sys.stdout.write('\r{}'.format(cursor))
-                sys.stdout.flush()
 
 class XPcoin():
     difficulty = 3
@@ -98,21 +57,24 @@ class XPcoin():
         pass
         # txn = rec.pk + amount + self.sk
 
-
     # TODO: What if transactions == None?
     @threaded
     def grind_xp(self, value, userID):
-        testHash = encryptor.encrypt(str(json.dumps(transactions, sort_keys=True)) + str(value))
-        code = '0' * difficulty
-        if testHash[0:difficulty] == code:
-            self.minted = True
-            # add txn with user gaining free money
-            self.minerID = userID
-            self.levelUP()
+        if self.transactions is None:
+            # add free monies
+            pass
+        else:
+            testHash = encryptor.encrypt(str(json.dumps(self.transactions, sort_keys=True)) + str(value))
+            code = '0' * difficulty
+            if testHash[0:difficulty] == code:
+                self.minted = True
+                # add txn with user gaining free money
+                self.minerID = userID
+                self.levelUP()
 
     def levelUP(self):
         self.contents.update({u'timestamp':timer.time(), u'numTxn':len(self.transactions), u'transactions':self.transactions})
         hash = encryptor.encrypt(self.contents)
-        return {u'hash':hash, u'contents':contents}
+        return {u'hash':hash, u'contents':self.contents}
 
 
