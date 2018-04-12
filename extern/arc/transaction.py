648b2bd48a6431f9c2b3120a7aff4207e932b42c
Navras Kamal <32576299+NavrasK@users.noreply.github.com>
2018-04-12 00:27:14 -0600

    added some documentation, updated the function to save the private key

--- extern/arc/transaction.py
+++ extern/arc/transaction.py
@@ -6,7 +6,7 @@ class Transaction():
         self.recipient = recipient
         self.quantity = quantity
 
-# NOTE: Transactions are signed by the private key of the sender, and can be decrypted with THEIR
-#       public key only, thus they are guarenteed to be the sender of the transaction, and as
+# NOTE: Transactions are to be signed by the private key of the sender, and can be decrypted with
+#       THEIR public key only, thus they are guarenteed to be the sender of the transaction, and as
 #       the only person who can send a transaction is the one losing money you cannot make false
 #       transactions in someone else's name
--- extern/arc/xp.py
+++ extern/arc/xp.py
@@ -2,9 +2,9 @@ import time
 
 class XP():
     def __init__(self, ind, prev_hash, data, hash, nonce):
-        self.index = ind
-        self.prev_hash = prev_hash()
-        self.timestamp = time.time()
-        self.data = data
-        self.hash = hash
-        self.nonce = nonce
+        self.index = ind                # This blocks index on the blockchain
+        self.prev_hash = prev_hash()    # The previous blocks hash
+        self.timestamp = time.time()    # The time this block was created
+        self.data = data                # Contains a list of transaction objects
+        self.hash = hash                # This blocks hash, following the proof of work
+        self.nonce = nonce              # The nonce that created the accepted hash for this block
