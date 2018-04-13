b0bde01dd53ea6848fb5473710f5ef0e9a23af96
Navras Kamal <32576299+NavrasK@users.noreply.github.com>
2018-04-13 11:08:23 -0600

    better imports

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
 # Store external functions in the extern folder, in their proper category.  To reference those files do
 # from extern.GROUP.FILE import FUNCTION/CLASS
 
-import extern.arc.transaction
-import extern.arc.xp
-import extern.enc.encrypt
-import extern.gui.interface
-import extern.net.network
-import extern.usr.user
-import extern.mis.misc
+import extern.arc.transaction as transaction
+import extern.arc.xp as xp
+import extern.enc.encrypt as encrypt
+import extern.gui.interface as gui
+import extern.net.network as network
+import extern.usr.user as u
+import extern.mis.misc as misc
 
 # NOTE: All files in extern should be totally independent of anything, or at least anything outside
 #       of their group.  Their interconnection should be totally built in this file
