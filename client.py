e90b6dffd86b62934d7ba60d6754a383e2bb150a
Navras Kamal <32576299+NavrasK@users.noreply.github.com>
2018-04-14 18:37:47 -0600

    mostly comments

--- client.py
+++ client.py
@@ -7,10 +7,13 @@ import extern.arc.transaction as txn
 import extern.arc.xp as xp
 import extern.arc.blockchain as chain
 import extern.enc.encrypt as k
-import extern.gui.interface as gui
+#import extern.gui.interface as gui # Not sure why this is happening, but fix GUI first then fix this later
 import extern.net.network as net
 import extern.usr.user as u
 import extern.mis.misc as misc
+
+import tkinter
+import time
 
 # NOTE: All files in extern should be totally independent of anything, or at least anything outside
 #       of their group.  Their interconnection should be totally built in this file
@@ -19,3 +22,11 @@ class Client():
     def __init__(self, iswin):
         print("CLIENT INITIALIZED")
         self.iswindows = iswin # Host operating system, True if running on windows, False otherwise
+        self.user = u
+        self.main()
+
+    def main(self):
+        # This is the main loop of the program
+        while True:
+            print("MAIN")
+            time.sleep(1)
--- extern/usr/user.py
+++ extern/usr/user.py
@@ -14,6 +14,8 @@ class User():
         print("User Created")
 
     def login(self):
+        # Checks if the user has a folder, if not it makes one and generates a new set of keys
+        # then it wil "log in" the user
         pathlib.Path(self.dir).mkdir(exist_ok=True)
         with open(self.dir+"/private.txt") as file:
             file.seek(0)
@@ -27,11 +29,14 @@ class User():
         self.getPublicId()
 
     def newUser(self):
+        # Generates a new key pair for the user and stores the public key in the "server"
+        # paired with an encrypted version of their private key so they can grab it later
         self.id_key, self.p_key = rsa.newkeys(2048)
         with open("../../../files/users/publickeys.txt", 'a') as file:
             file.write(hashlib.sha256(self.p_key+"EXPOSURE")+":"+self.id_key)
     
     def getPublicId(self):
+        # Searches the "key server" to find the active user's public key
         with open("../../../files/users/publickeys.txt", 'r') as file:
             for line in file:
                 hashedkey, public = line.split(':')
